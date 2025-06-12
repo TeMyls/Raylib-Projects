fb4770615dc772b5fafc49f9009f43266b60fc7a
Terrance Myles Jr <tlmyles5@hotmail.com>
2025-06-12 10:26:56 -0500

    Add files via upload

--- Basic Window/main.c
+++ Basic Window/main.c
@@ -1,1 +1,16 @@
+#include "raylib.h"
 
+int main(void) {
+    InitWindow(800, 450, "Hello, Raylib!");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Hello, Raylib!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
